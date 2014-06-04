fef17e8ce51e2fbaeebda23b035d60cba6a43a91
Khirod <khirod2324@gmail.com>
2014-06-04 23:57:32 +0530

    renamed package

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-	name = "tagz",
+	name = "ktagz",
 	packages = find_packages(),
 	version = "0.1.0",
-	description = "A command line file-tagger for linux",
+	description = "A command line file-tagger/file-search tool for linux",
+	long_description = 'Basically it can help you with tagging files and then you can easily search files with a particular tag using text search.',
 	author = "Khirod Kant Naik",
 	author_email = "khirod234@gmail.com",
 	url='https://github.com/shinigamiryuk/TagZ',
