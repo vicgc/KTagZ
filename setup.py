d4609b8d70116b3f756699c05c8514682972fb1a
Khirod <khirod2324@gmail.com>
2014-06-04 15:26:21 +0530

    Setup File

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = "TagZ",
+	version = "0.1",
+	description = "A command line file-tagger for linux",
+	author = "Khirod Kant Naik",
+	author_email = "khirod234@gmail.com",
+	url = "kkantnaik.blogspot.in",
+	py_modules = ["tagz.py", "query_code.py"],
+	scripts = ["tagz"],
+)
