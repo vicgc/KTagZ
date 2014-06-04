e9ff65882a0d7dab53d3e5f1f28cd83376a5ec18
Khirod <khirod2324@gmail.com>
2014-06-04 14:57:06 +0530

    Basic querying

--- query_code.py
+++ query_code.py
@@ -1,4 +1,19 @@
 from elasticsearch import Elasticsearch
+import json
 
 es = Elasticsearch()
-print es.get(index='tagz', doc_type='files')
+
+doc = {
+	"query": {
+		"query_string": {
+			"query": "github fuck readme tutorial"
+		}
+	}
+}
+
+res = es.search(index='tagz', doc_type='files', body=doc)
+lst = (res['hits'])['hits']
+
+for i in range(0, len(lst)):
+	print json.dumps( (lst[i])['_source'], sort_keys=True, indent=4, separators=(',', ': ') )
+
