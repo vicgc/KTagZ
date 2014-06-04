bfc37ba18562b7dc0a0ad049e15b61a6017a665c
Khirod <khirod2324@gmail.com>
2014-06-04 11:29:16 +0530

    Initial commit

---
+++ tagz.py
@@ -0,0 +1,43 @@
+import argparse
+import os, json
+from elasticsearch import Elasticsearch
+import uuid
+
+parser = argparse.ArgumentParser()
+
+# File to be indexed is a positional argument
+parser.add_argument("file_name", help="file to be indexed")
+
+# Currently only optional argument support (later add optional arguments using subparsers)
+parser.add_argument("-t","--tags", help="Adds tags to file", action="store_true")
+parser.add_argument("-d", "--desc", help="Adds description to a file", action="store_true")
+args = parser.parse_args()
+
+tags = ""
+desc = ""
+
+if args.tags:
+	tags = raw_input("Tags [separated by a space]: ")
+
+if args.desc:
+	desc = raw_input("Description [short description]: ")
+
+	
+name = args.file_name
+tags = tags.split(' ')
+path = os.getcwd() + '/' + args.file_name
+
+doc = {
+	"file": name,
+	"path": path,
+	"tags": tags,
+	"description": desc
+}
+
+unique_id = str(uuid.uuid4())
+
+try:
+	es = Elasticsearch()
+	es.index(index='tagz', doc_type='files', id=unique_id, body=doc)
+except:
+	print 'Oops!. Encountered an error'
