4eb71dc1f1e97c1f02ba048cb5a0c1cec5cddadb
Khirod <khirod2324@gmail.com>
2014-06-06 18:48:27 +0530

    Awesome changes

--- setup.py
+++ setup.py
@@ -4,14 +4,11 @@ from setuptools import find_packages
 setup(
 	name = "ktagz",
 	packages = find_packages(),
-	version = "0.1.3",
+	version = "0.1.6",
 	description = "A command line file-tagger/file-search tool for linux",
 	long_description = open('README.md').read(),
 	author = "Khirod Kant Naik",
 	author_email = "khirod234@gmail.com",
 	url='https://github.com/shinigamiryuk/TagZ',
-	scripts = ['bin/ktagz', 'bin/createIndex.sh'],
-	install_requires=[
-	"elasticsearch >= 1.0.0",
-	]
+	scripts = ['bin/ktagz'],
 )
